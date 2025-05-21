b6525b78a1d870aeea460ea9722b87840d44feb0
cdeets24 <deetscaleb@gmail.com>
2025-05-20 21:14:59 -0400

    Create Area_Calculator.py

---
+++ Area_Calculator.py
@@ -0,0 +1,35 @@
+import math
+
+pi = math.pi
+
+print ("Which shape would you like to find the area of. Please enter the numeric value of the options below:")
+print ("""1) Square
+       2) Rectangle
+       3) Triangle
+       4) Circle""")
+input = ("Please choose a shape:    ")
+
+if input == 1:
+    side = int(input("Please enter the length of the square side.   "))
+    
+    area = side ** 2
+    print ("The area of the square is " + area)
+elif input == 2:
+    length = int(input("Please enter the length of the rectangle."))
+    width = int(input("Please enter the width of the rectangle."))
+
+    area = length * width
+    print ("The area of the rectangle is " + area)
+elif input == 3:
+    height = int(input("Please enter the height of the triangle."))
+    base = int(input("Please enter the base length of the triangle."))
+
+    area = (height * base) / 2
+    print ("The area of the triangle is " + area)
+elif input == 4:
+    radius = int(input("Please enter the radius of the circle."))
+
+    area = pi * radius ** 2
+    print ("The area of the circle is " + area)
+else:
+    print (("Please enter the numeric value of the shape you want to calculate."))
